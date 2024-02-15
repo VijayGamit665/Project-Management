ed3bc9fcc0a7ede0227c2a24d9057d266a009acf
VijayGamit665 <vijaygamit665@gmail.com>
2024-02-15 21:17:26 +0530

    Admin dash Board

---
+++ src/main/java/com/grownited/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.grownited.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+	
+	@GetMapping("/adminDashboard")
+	public String adminDashboard() {
+		return "AdminDashBoard";
+	}
+	
+}
